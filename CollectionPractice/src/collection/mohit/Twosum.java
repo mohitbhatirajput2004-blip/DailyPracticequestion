dae4cfa6bf9f38b926d5b9289ac1f81050b1fbd0
Mohit <mohitbhatrajput2004@gmail.com>
2025-12-05 13:45:58 +0530

    TwoSum leet code problem 1 optimized Approach

--- CollectionPractice/src/collection/mohit/Twosum.java
+++ CollectionPractice/src/collection/mohit/Twosum.java
@@ -2,8 +2,8 @@ package collection.mohit;
 // given an array , find  two numbers that has 50 sum .
 public class Twosum {
 	public static void main(String[] args) {
-		int []arr= {20,10,25,40,55,60,14,36};
-		int target=50;
+		int []arr= {10,15,18,28,30,40,50};
+		int target=70;
 		for(int i=0;i<arr.length;i++) {
 			int x= target-arr[i];
 			for(int j=i+1;j<arr.length;j++) {
