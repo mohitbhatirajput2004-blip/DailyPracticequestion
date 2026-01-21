2595c94f4245cf71679f52f015e456be0ac67427
Mohit <mohitbhatrajput2004@gmail.com>
2026-01-21 12:14:44 +0530

    conversion for array and list in collection

--- CollectionPractice/src/collection/mohit/ConversionList.java
+++ CollectionPractice/src/collection/mohit/ConversionList.java
@@ -8,45 +8,58 @@ import java.util.Set;
 
 public class ConversionList {
 	public static void main(String[] args) {
-//		String arr[] = {"java" , "python" , "cpp" , "java"};
+		 //array to List 
 //		
-//		List<String> l = new ArrayList<>(Arrays.asList(arr));		
-//		l.add("hii");
+//		String arr[] = {"HI" , "OK" , "HI"};
+//		
+//		//Arrays.asList(arr)
+//		
+//		List <String> l = Arrays.asList(arr);
+//	//	l.add("abs");
+//		
 //		System.out.println(l);
 //		
-//		Set<String> s = new HashSet<>(Arrays.asList(arr));
-//		s.add("okkkkkkkk");
+//		List <String> l1 = new ArrayList<>(Arrays.asList(arr));
+//		l1.add("abc");
+//		
+//		System.out.println(l1);
+//		
+//		Set <String> s = new HashSet<>(Arrays.asList(arr));
 //		
 //		System.out.println(s);
 		
-		List<String> l = new ArrayList<>();
-		l.add("jii");
-		l.add("ojksj");
-		l.add("sgdjhs");
+		//List to array 
+		List<Integer> l = new ArrayList<>();
+		l.add(10);
+		l.add(20);
+		l.add(30);
+		l.add(40);
 		
 		
-	
-		String[] arr = l.toArray(new String[0]);
+		Integer arr[] = l.toArray(new Integer[0]);
 		
-		for(String a : arr) {
+		for(int a : arr) {
 			System.out.println(a);
 		}
-		
+		System.out.println(Arrays.toString(arr));
 		
 		
 		Set<Integer> s = new HashSet<>();
-		s.add(10);
-		s.add(20);
-		s.add(30);
-		s.add(40);
-		s.add(50);
+		s.add(1);
+		s.add(2);
+		s.add(3);
+		s.add(4);
 		
 		Integer arr2[] = s.toArray(new Integer[0]);
 		
-		
-		for(Integer a :  arr2) {
+		for(int a: arr2) {
 			System.out.println(a);
 		}
-	}
 		
+		
+		}
+	
+	
+
 }
+
